8814fd97110345b106696ac6a6d09b8f9e8d7328
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-02 02:39:16 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ let roleHarvester = require('role.harvester');
 let roleUpgrader = require('role.upgrader');
 let roleBuilder = require('role.builder');
 
+const Role = {
+    HARVESTER: 0,
+    BUILDER: 1,
+    UPGRADER: 2,
+    
+};
+
+const roleBodyMap = new Map([
+    []
+]);
+function calculateCost(bodyParts){
+    let costMap = new Map([
+        [MOVE, 50],
+        [WORK, 100],
+        [CARRY, 50],
+        [ATTACK, 80],
+        [RANGED_ATTACK, 150],
+        [HEAL, 250],
+        [CLAIM, 600],
+        [TOUGH, 10]
+    ]);
+
+    let cost = 0;
+    for(let bodyPart of bodyParts){
+        cost += costMap.get(bodyPart);
+    }
+    return cost;
+}
+
 module.exports.loop = function () {
     /*
     let tower = Game.getObjectById('TOWER_ID');
@@ -19,6 +48,15 @@ module.exports.loop = function () {
         }
     }
     */
+
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    
 
     for(let name in Game.creeps) {
         let creep = Game.creeps[name];
