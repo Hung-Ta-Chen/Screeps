328a9e041a49aa8c7ce831973c6b55865a836310
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-03 01:42:32 -0700

    Create role.defender.js

---
+++ role.defender.js
@@ -0,0 +1,11 @@
+let defenderRole = {
+    run: function(creep) {
+        let target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (target) {
+            if (creep.attack(target) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(target);
+            }
+        }
+    }
+};
+module.exports = defenderRole;
