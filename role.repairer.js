71f22183245d3ebec1855b5d038c6f73c301ebad
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-03 05:11:43 -0700

    Update role.repairer.js

--- role.repairer.js
+++ role.repairer.js
@@ -1,13 +1,17 @@
 let roleRepairer = {
-  
+ 
     run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
-            let source = creep.pos.findClosestByRange(FIND_SOURCES);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+      
+        if(creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.repairing = false;
+            creep.say('🔄 harvest');
         }
-        else {
+        if(!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
+            creep.memory.repairing = true;
+            creep.say('🔧 repair');
+        }
+    
+        if(creep.memory.repairing) {
             let targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     // Exclude walls and roads from repair targets
@@ -18,16 +22,20 @@ let roleRepairer = {
                     );
                 }
             });
-          
-            if(targets.length > 0) {
+            if(targets.length) {
                 if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
             else{
-                // Rest at (40, 40)
                 creep.moveTo(40, 40);
-            }  
+            }
+        }
+        else {
+            let source = creep.pos.findClosestByRange(FIND_SOURCES);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
         }
     }
 };
