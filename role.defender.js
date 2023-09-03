f4160e99ef4a6931aa35a282a3c26ae45e2cd3c8
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-03 07:44:56 -0700

    Update role.defender.js
    Add patrol action to defenser role

--- role.defender.js
+++ role.defender.js
@@ -11,9 +11,31 @@ let roleDefender = {
         }
         else{
             // Randomly move to a new position if no enemy found
+            /*
             let xPos = Math.floor(Math.random() * 50);
             let yPos = Math.floor(Math.random() * 50);
             creep.moveTo(xPos, yPos);
+            */
+            // If no hostile creeps are present, patrol between predefined waypoints
+            let waypoints = [{ x: 12, y: 10 }, 
+                             { x: 9, y: 27 }, 
+                             { x: 13, y: 44 }, 
+                             { x: 27, y: 38 }, 
+                             { x: 43, y: 35 }, 
+                             { x: 39, y: 22 },
+                             { x: 24, y: 19 },
+                            ];
+            let currentWaypoint = creep.memory.waypoint || 0;
+
+            if (creep.pos.isEqualTo(waypoints[currentWaypoint].x, waypoints[currentWaypoint].y)) {
+                currentWaypoint = (currentWaypoint + 1) % waypoints.length;
+            }
+
+            // Store the index of current waypoint in memory
+            creep.memory.waypoint = currentWaypoint;
+
+            // Move toward the current waypoint
+            creep.moveTo(waypoints[currentWaypoint].x, waypoints[currentWaypoint].y);
         }
     }
 };
