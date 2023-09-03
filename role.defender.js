7712147e6557296e67a649dc988d79f6657f7a4d
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-03 02:28:40 -0700

    Update role.defender.js

--- role.defender.js
+++ role.defender.js
@@ -4,9 +4,16 @@ let roleDefender = {
     run: function(creep) {
         let target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (target) {
+            // Attack the enemy if found
             if (creep.attack(target) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(target);
             }
+        }
+        else{
+            // Randomly move to a new position if no enemy found
+            let xPos = Math.floor(Math.random() * 50);
+            let yPos = Math.floor(Math.random() * 50);
+            creep.moveTo(xPos, yPos);
         }
     }
 };
