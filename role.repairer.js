acb67390de9c93d02c1d6c17e030c14554a6413b
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-03 04:44:15 -0700

    Update role.repairer.js

--- role.repairer.js
+++ role.repairer.js
@@ -1,12 +1,28 @@
 let roleRepairer = {
   
     run: function(creep) {
-        let target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if (target) {
-            if (creep.repair(target) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
+        if(creep.store.getFreeCapacity() > 0) {
+            let source = creep.pos.findClosestByRange(FIND_SOURCES);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else {
+            let targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    // Exclude walls and roads from repair targets
+                    return (
+                        structure.hits < structure.hitsMax &&
+                        structure.structureType !== STRUCTURE_WALL &&
+                        structure.structureType !== STRUCTURE_ROAD
+                    );
+                }
+            });
+          
+            if(targets.length > 0) {
+                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
         }
     }
