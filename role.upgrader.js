94fb83b90398479a8ee5ff5958ba6ec546062a94
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-03 19:59:43 -0700

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,27 +2,58 @@ let roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        // Check if there are any harvesters in the room
+        let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
 
-        if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
-            creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
-        }
-        if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
-            creep.memory.upgrading = true;
-            creep.say('⚡ upgrade');
-        }
-
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+        if(harvesters === 0){
+            // Work as a harvester if no harvester is present
+            creep.say('Work as a harvester!');
+            if(creep.store.getFreeCapacity() > 0) {
+                let source = creep.pos.findClosestByRange(FIND_SOURCES);
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
+            }
+            else {
+                let targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_TOWER) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
             }
         }
-        else {
-            let sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+        else{
+            // Do upgrader's job
+            if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+                creep.memory.upgrading = false;
+                creep.say('🔄 harvest');
+            }
+            if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+                creep.memory.upgrading = true;
+                creep.say('⚡ upgrade');
+            }
+    
+            if(creep.memory.upgrading) {
+                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+            else {
+                let sources = creep.room.find(FIND_SOURCES);
+                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+                }
             }
         }
+        
     }
 };
 
