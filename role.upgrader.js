a6a4872034136ab3b04c68a4d2eb44861bed2ec8
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-04 05:09:38 -0700

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,3 +1,5 @@
+let roleHarvester = require("role.harvester");
+
 let roleUpgrader = {
 
     /** @param {Creep} creep **/
@@ -6,29 +8,7 @@ let roleUpgrader = {
         let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role === 'harvester');
 
         if(harvesters.length <= 2){
-            // Work as a harvester if no harvester is present
-            creep.say('🔄 HARVEST');
-            if(creep.store.getFreeCapacity() > 0) {
-                let source = creep.pos.findClosestByRange(FIND_SOURCES);
-                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
-            }
-            else {
-                let targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                            structure.structureType == STRUCTURE_SPAWN ||
-                            structure.structureType == STRUCTURE_TOWER) &&
-                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                    }
-                });
-                if(targets.length > 0) {
-                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                    }
-                }
-            }
+            roleHarvester.run(creep);
         }
         else{
             // Do upgrader's job
