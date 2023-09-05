e2af969afe6741c11b366203d8a5d43d601fb4aa
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-05 03:07:21 -0700

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,51 @@
+// Define constants for roles
+export const Role = {
+    HARVESTER: 0,
+    HAULER: 1,
+    BUILDER: 2,
+    UPGRADER: 3,
+    DEFENDER: 4,
+    REMOTE_HARVESTER: 5,
+    SCOUT: 6,
+    CLAIMER: 7,
+    MINER: 8,
+    REPAIRER: 9,
+    RESERVER: 10,
+    ATTACKER: 11,
+};
+
+// Define a map between the name of a role and its corresponding costant
+export const roleNameConstantMap = {
+    'harvester': Role.HARVESTER,
+    'hauler': Role.HAULER,
+    'builder': Role.BUILDER,
+    'upgrader': Role.UPGRADER,
+    'defender': Role.DEFENDER,
+    'remote harvester': Role.REMOTE_HARVESTER,
+    'scout': Role.SCOUT,
+    'claimer': Role.CLAIMER,
+    'miner': Role.MINER,
+    'repairer': Role.REPAIRER,
+    'reserver': Role.RESERVER,
+    'attacker': Role.ATTACKER,
+};
+
+// Calculate the cost of creating a creep
+export function calculateCost(bodyParts){
+    let costMap = new Map([
+        [MOVE, 50],
+        [WORK, 100],
+        [CARRY, 50],
+        [ATTACK, 80],
+        [RANGED_ATTACK, 150],
+        [HEAL, 250],
+        [CLAIM, 600],
+        [TOUGH, 10],
+    ]);
+
+    let cost = 0;
+    for(let bodyPart of bodyParts){
+        cost += costMap.get(bodyPart);
+    }
+    return cost;
+}
