b162dd7667efcc24ba0d897e4999bd4561223c80
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-05 03:49:13 -0700

    Update main.js
    Add a new mode to the main loop

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 let modePhase1 = require("mode.phase1");
+let modePhase2 = require("mode.phase2");
+
+const currentRoom = Game.spawns["Spawn1"].room;
+let controllerLevel = 0;
+let totalEnergyCapacity = 0;
+
+if (currentRoom.controller) {    
+    controllerLevel = currentRoom.controller.level;
+
+    const extensions = room.find(FIND_STRUCTURES, {
+        filter: (structure) => structure.structureType === STRUCTURE_EXTENSION
+    });
+    const extensionsEnergyCapacity = extensions.reduce((total, extension) => {
+        return total + extension.store.getCapacity(RESOURCE_ENERGY);
+    }, 0);
+    
+    totalEnergyCapacity = extensionsEnergyCapacity + Game.spawns["Spawn1"].getCapacity(RESOURCE_ENERGY);
+}
+
 
 // Main loop
-module.exports.loop = modePhase1;
+if(controllerLevel >= 3 && totalEnergyCapacity >= 400){
+    module.exports.loop = modePhase2;
+}
+else{
+    module.exports.loop = modePhase1;
+}
