9acfc606ceb5f0cf8c963acaf0ff0104a48cb675
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-05 02:25:05 -0700

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -3,9 +3,9 @@ let roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
         if(creep.store.getFreeCapacity() > 0) {
-            let source = creep.pos.findClosestByRange(FIND_SOURCES);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            let sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
         else {
