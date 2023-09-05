bc0632cfef0ea4596f0525329726dae2046a2238
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-05 00:13:40 -0700

    Update main.js
    Assign a priority to each role and generate a new creep based on it

--- main.js
+++ main.js
@@ -20,7 +20,23 @@ const Role = {
     ATTACKER: 11,
 };
 
-// Body parts of each role
+// Define a map between the name of a role and its corresponding costant
+const roleNameConstantMap = {
+    'harvester': Role.HARVESTER,
+    'hauler': Role.HAULER,
+    'builder': Role.BUILDER,
+    'upgrader': Role.UPGRADER,
+    'defender': Role.DEFENDER,
+    'remote harvester': Role.REMOTE_HARVESTER,
+    'scout': Role.SCOUT,
+    'claimer': Role.CLAIMER,
+    'miner': Role.MINER,
+    'repairer': Role.REPAIRER,
+    'reserver': Role.RESERVER,
+    'attacker': Role.ATTACKER,
+};
+
+// Define body parts of each role
 const roleBodyMap = new Map([
     [Role.HARVESTER, [WORK, CARRY, MOVE]],
     [Role.HAULER, [CARRY, CARRY, MOVE]],
@@ -56,6 +72,16 @@ function calculateCost(bodyParts){
     return cost;
 }
 
+// Define the target number of each role
+const roleTargetCount = {
+    'upgrader': 4,
+    'harvester': 6,
+    'builder': 5,
+    'defender': 4,
+    'repairer': 2, 
+};
+
+
 // Main loop
 module.exports.loop = function () {
     /*
@@ -87,59 +113,32 @@ module.exports.loop = function () {
     }
 
 
-    // Check the number of repairers
-    let n_repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+    // Calculate the priority of each role based on their current count
+    let rolePriorityList = {};
+    for(let role in roleTargetCount){
+        let roleCount = _.filter(Game.creeps, (creep) => creep.memory.role == role);
+        let rolePriority = roleTargetCount[role] - roleCount;
 
-    if(n_repairers.length < 2 && Game.spawns["Spawn1"].room.energyAvailable >= calculateCost(roleBodyMap.get(Role.REPAIRER)) && !Game.spawns["Spawn1"].spawning) {
-        var newName = 'Repairer' + Game.time;
-        console.log('Spawning new repairer: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep(roleBodyMap.get(Role.REPAIRER), newName,
-            {memory: {role: 'repairer'}});
+        rolePriorityList[role] = rolePriority;
     }
 
-   
-    // Check the number of builders
-    let n_builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-
-    if(n_builders.length < 8 && Game.spawns["Spawn1"].room.energyAvailable >= calculateCost(roleBodyMap.get(Role.BUILDER)) && !Game.spawns["Spawn1"].spawning) {
-        var newName = 'Builder' + Game.time;
-        console.log('Spawning new builder: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep(roleBodyMap.get(Role.BUILDER), newName,
-            {memory: {role: 'builder'}});
+    // Generate a creep of the role with the highest priority
+    let highestPriority = -1000;
+    let highestPriorityRole = null;
+    for(let role in roleTargetCount){
+        if(rolePriorityList[role] > highestPriority){
+            highestPriority = rolePriorityList[Role];
+            highestPriorityRole = role;
+        }
     }
 
-    
-    // Check the number of defenders
-    let n_defenders = _.filter(Game.creeps, (creep) => creep.memory.role == 'defender');
-
-    if(n_defenders.length < 5 && Game.spawns["Spawn1"].room.energyAvailable >= calculateCost(roleBodyMap.get(Role.DEFENDER)) && !Game.spawns["Spawn1"].spawning) {
-        var newName = 'Defender' + Game.time;
-        console.log('Spawning new defender: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep(roleBodyMap.get(Role.DEFENDER), newName,
-            {memory: {role: 'defender'}});
+    if(highestPriorityRole && Game.spawns["Spawn1"].room.energyAvailable >= calculateCost(roleBodyMap.get(roleNameConstantMap[highestPriorityRole]))){
+        var newName = highestPriorityRole + Game.time;
+        console.log('Spawning new ' + highestPriorityRole + ': ' + newName);
+        Game.spawns['Spawn1'].spawnCreep(roleBodyMap.get(roleNameConstantMap[highestPriorityRole]), newName,
+            {memory: {role: highestPriorityRole}});
     }
 
-    
-    // Check the number of upgraders
-    let n_upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-
-    if(n_upgraders.length < 5 && Game.spawns["Spawn1"].room.energyAvailable >= calculateCost(roleBodyMap.get(Role.UPGRADER)) && !Game.spawns["Spawn1"].spawning) {
-        var newName = 'Upgrader' + Game.time;
-        console.log('Spawning new upgrader: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep(roleBodyMap.get(Role.UPGRADER), newName,
-            {memory: {role: 'upgrader'}});
-    }
-
-
-    // Check the number of harvesters
-    let n_harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-
-    if(n_harvesters.length < 6 && Game.spawns["Spawn1"].room.energyAvailable >= calculateCost(roleBodyMap.get(Role.HARVESTER)) && !Game.spawns["Spawn1"].spawning) {
-        var newName = 'Harvester' + Game.time;
-        console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep(roleBodyMap.get(Role.HARVESTER), newName,
-            {memory: {role: 'harvester'}});
-    }
 
     let numString = `Harvesters: ${n_harvesters.length}, ` + 
                     `Defenders: ${n_defenders.length}, ` + 
@@ -148,6 +147,7 @@ module.exports.loop = function () {
                     `Builders: ${n_builders.length}`;
     console.log(numString);
 
+    
     // Make each creep do its coeesponding job
     for(let name in Game.creeps) {
         let creep = Game.creeps[name];
