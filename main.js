586bf527b186592c866e55467659b04a02836237
Hung-Ta Chen <htcchen@ucdavis.edu>
2023-09-06 00:57:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,35 @@
-let modePhase1 = require("mode.phase1");
-let modePhase2 = require("mode.phase2");
-let modeDefense = require("mode.defense");
-
-const currentRoom = Game.spawns["Spawn1"].room;
-let controllerLevel = 0;
-let totalEnergyCapacity = 0;
-
-if (currentRoom.controller) {    
-    // Get the level of the controller
-    controllerLevel = currentRoom.controller.level;
-
-    // Calculate the max energy capacity
-    const extensions = currentRoom.find(FIND_STRUCTURES, {
-        filter: (structure) => structure.structureType === STRUCTURE_EXTENSION
-    });
-    const extensionsEnergyCapacity = extensions.reduce((total, extension) => {
-        return total + extension.store.getCapacity(RESOURCE_ENERGY);
-    }, 0); 
-    totalEnergyCapacity = extensionsEnergyCapacity + Game.spawns["Spawn1"].store.getCapacity(RESOURCE_ENERGY);
-}
-
-// Detect hostile creeps
-const hostileCreeps = currentRoom.find(FIND_HOSTILE_CREEPS);
-
-
-// Main loop
-if(hostileCreeps.length > 0){
-    // Enter defense mode if any hostile creep detected
-    module.exports.loop = modeDefense;
-}
-else{
-    if(controllerLevel >= 3 && totalEnergyCapacity >= 400){
-        module.exports.loop = modePhase2;
+module.exports.loop = function () {
+    /*
+    // Remove rooms no longer visible
+    for (const roomName in Memory.rooms) {
+        if (!Game.rooms[roomName]) {
+            delete Memory.rooms[roomName];
+        }
     }
-    else{
-        module.exports.loop = modePhase1;
+    */
+    
+    // Initialize rooms in Memory if they are not already
+    for (const roomName in Game.rooms) {
+        if (!Memory.rooms[roomName]) {
+            Memory.rooms[roomName] = {
+                manager: {
+                    module: 'roomManager',
+                    enabled: true, 
+                },
+                controllerLevel: Game.rooms[roomName].controller.level,
+                mode: 0,
+            };
+        }
     }
-}
 
+    // Iterate over rooms and call room-specific managers
+    for (const roomName in Memory.rooms) {
+        const roomMemory = Memory.rooms[roomName];
+
+        // Check if the room has a manager and it's enabled
+        if (roomMemory.manager && roomMemory.manager.enabled) {
+            const managerModule = require(roomMemory.manager.module);
+            managerModule.run(roomName);
+        }
+    }
+};
